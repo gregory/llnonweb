1f1fde13fa6f79029ba4c276a75006790a9e68db
Horion Grégory <greg2502@gmail.com>
2011-05-05 00:00:39 +0200

    implementing oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'opdvvW87GLaUrepyujPg', 'O4NHyNfhpvdkFq8cpcOi9zmYRR5gahc16riviFWPbE'
+  #provider :twitter, 'opdvvW87GLaUrepyujPg', 'O4NHyNfhpvdkFq8cpcOi9zmYRR5gahc16riviFWPbE'
+  provider :facebook, "147749211905466", "b682557c3bc08512d09ebc4b848d8d93"
+    provider :twitter, "fno1nY6Vx8G80mNFXKPukg", "UnTQrcu2nPG5bqTS3KYdRPkQ2wKgSZz4uBC1RRpRD0"
 end
