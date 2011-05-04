85b62581bd19014ebbbd9f198a8f1136763ef610
Horion Grégory <greg2502@gmail.com>
2011-05-05 00:23:12 +0200

    implementing oauth

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,14 +1,17 @@
 class AuthenticationsController < ApplicationController
   def index
-    @authentications = Authentication.all
+    @authentications = current_user.authentications if current_user
   end
 
   def create
-    render :text => request.env["omniauth.auth"].to_yaml
+    auth =request.env["omniauth.auth"]
+    current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
+    flash[:notice] = "Authentication succefull"
+    redirect_to authentications_url
   end
 
   def destroy
-    @authentication = Authentication.find(params[:id])
+    @authentication = current_user.authentications.find(params[:id])
     @authentication.destroy
     redirect_to authentications_url, :notice => "Successfully destroyed authentication."
   end
