edea0ef2604f3f07b781d7c96033b049e2166206
Horion Grégory <greg2502@gmail.com>
2011-05-05 02:27:05 +0200

    improved authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
+  respond_to :html, :xml, :json
   def index
     @pages = Page.all
+    respond_with(@pages)
   end
 
   def show
     @page = Page.find(params[:id])
+    
+    respond_with(@page)
   end
 
   def new
